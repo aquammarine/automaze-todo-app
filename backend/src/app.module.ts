8d03374f14731aa538ea3468883cad6c5054e78a
aquammarine <mrguysy@gmail.com>
2026-07-15 19:06:12 +0300

    fix: prisma and redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './infra/prisma/prisma.module';
+import { PrismaModule } from './infra/database/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { RedisModule } from './infra/cache/redis/redis.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, RedisModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
