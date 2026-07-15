26f0c7eaf3ab346ad7c74ea761cd167ae83969af
aquammarine <mrguysy@gmail.com>
2026-07-15 22:06:52 +0300

    feat: add shadcn components

---
+++ frontend/src/shared/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { Button } from "./button";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "./card";
+export { Checkbox } from "./checkbox";
+export { Label } from "./label";
+export { Toaster } from "./sonner";
