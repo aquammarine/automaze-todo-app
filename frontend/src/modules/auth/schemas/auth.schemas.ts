6846128c0c3422eeb850cf6b517b6958438207da
aquammarine <mrguysy@gmail.com>
2026-07-16 02:14:23 +0300

    feat: add schema for auth

---
+++ frontend/src/modules/auth/schemas/auth.schemas.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(12, "Password must be at least 12 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
