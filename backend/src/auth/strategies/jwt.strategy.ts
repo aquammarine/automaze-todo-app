76fbb3d96bedd09d49fa4b88ccba807c2e58a90b
aquammarine <mrguysy@gmail.com>
2026-07-15 20:10:22 +0300

    fix: remove console log

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -12,7 +12,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: { id: string; email: string }) {
-    console.log('=============== JWT PAYLOAD === ', payload);
     return { id: payload.id, email: payload.email };
   }
 }
