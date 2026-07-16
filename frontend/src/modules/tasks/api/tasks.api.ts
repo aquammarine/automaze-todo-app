2ca8be04be3087a5d547cb3c24a0f02205833d55
aquammarine <mrguysy@gmail.com>
2026-07-16 18:21:40 +0300

    feat: add update task method

--- frontend/src/modules/tasks/api/tasks.api.ts
+++ frontend/src/modules/tasks/api/tasks.api.ts
@@ -1,6 +1,6 @@
 import { api } from "@/shared/lib/axios";
 import type { Task, TaskFilterParams } from "../types";
-import type { CreateTaskSchema } from "../schemas";
+import type { CreateTaskSchema, UpdateTaskSchema } from "../schemas";
 
 export const getTasks = async (filters?: TaskFilterParams): Promise<Task[]> => {
   const response = await api.get("/tasks", { params: filters });
@@ -9,5 +9,10 @@ export const getTasks = async (filters?: TaskFilterParams): Promise<Task[]> => {
 
 export const createTask = async (data: CreateTaskSchema): Promise<Task> => {
   const response = await api.post("/tasks", data);
+  return response.data;
+};
+
+export const updateTask = async (id: string, data: Partial<UpdateTaskSchema>): Promise<Task> => {
+  const response = await api.patch(`/tasks/${id}`, data);
   return response.data;
 };
