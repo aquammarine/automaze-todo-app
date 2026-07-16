50a878175ddf962715fc9b5f98146035ab745ac3
aquammarine <mrguysy@gmail.com>
2026-07-16 17:28:45 +0300

    fet: add placeholder data inside query

--- frontend/src/modules/tasks/hooks/useTasksQuery.ts
+++ frontend/src/modules/tasks/hooks/useTasksQuery.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getTasks } from "../api";
 import type { TaskFilterParams } from "../types";
 
@@ -6,5 +6,6 @@ export const useTasksQuery = (filters?: TaskFilterParams) => {
   return useQuery({
     queryKey: ["tasks", filters],
     queryFn: () => getTasks(filters),
+    placeholderData: keepPreviousData,
   });
 };
