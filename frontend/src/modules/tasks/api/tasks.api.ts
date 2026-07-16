aac905989cd27578ae495c3a12d065a8704e0855
aquammarine <mrguysy@gmail.com>
2026-07-16 16:45:51 +0300

    feat: add create task api method

--- frontend/src/modules/tasks/api/tasks.api.ts
+++ frontend/src/modules/tasks/api/tasks.api.ts
@@ -1,7 +1,13 @@
 import { api } from "@/shared/lib/axios";
 import type { Task, TaskFilterParams } from "../types";
+import type { CreateTaskSchema } from "../schemas";
 
 export const getTasks = async (filters?: TaskFilterParams): Promise<Task[]> => {
   const response = await api.get("/tasks", { params: filters });
+  return response.data;
+};
+
+export const createTask = async (data: CreateTaskSchema): Promise<Task> => {
+  const response = await api.post("/tasks", data);
   return response.data;
 };
