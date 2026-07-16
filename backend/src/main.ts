f9d8d7b62478daaf8cb4046076c9807df5ea41e2
aquammarine <mrguysy@gmail.com>
2026-07-16 13:00:13 +0300

    feat: add validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     origin: process.env.CLIENT_URL,
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
