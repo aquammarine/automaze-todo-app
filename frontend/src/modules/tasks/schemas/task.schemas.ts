67e21400ae017c00911b1660177242c849a0bcf7
aquammarine <mrguysy@gmail.com>
2026-07-17 20:59:38 +0300

    fix; missing field

--- frontend/src/modules/tasks/schemas/task.schemas.ts
+++ frontend/src/modules/tasks/schemas/task.schemas.ts
@@ -16,6 +16,7 @@ export const updateTaskSchema = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().optional(),
   priority: z.number().int().min(1).max(10),
+  completed: z.boolean().optional(),
 });
 
 export type UpdateTaskSchema = z.infer<typeof updateTaskSchema>;
