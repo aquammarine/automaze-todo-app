a457c9d2dc40f420a07a23781dd51c38c9921211
aquammarine <mrguysy@gmail.com>
2026-07-17 22:13:17 +0300

    fix: change redis setup to production ready

--- backend/src/infra/cache/redis/redis.service.ts
+++ backend/src/infra/cache/redis/redis.service.ts
@@ -6,11 +6,7 @@ export class RedisService extends Redis implements OnModuleDestroy {
   private readonly logger = new Logger(RedisService.name);
 
   constructor() {
-    super({
-      host: process.env.REDIS_HOST,
-      port: Number(process.env.REDIS_PORT),
-      maxRetriesPerRequest: null,
-    });
+    super(process.env.REDIS_URL!, { maxRetriesPerRequest: null });
 
     this.on('connect', () => {
       this.logger.log('Successfully connected to Redis!');
