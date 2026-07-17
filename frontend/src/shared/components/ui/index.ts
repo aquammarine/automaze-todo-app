1b79aa73b8068557435765b128ad1274df9174f9
aquammarine <mrguysy@gmail.com>
2026-07-17 19:20:18 +0300

    feat: add alert dialog

--- frontend/src/shared/components/ui/index.ts
+++ frontend/src/shared/components/ui/index.ts
@@ -85,3 +85,17 @@ export {
   AvatarGroupCount,
   AvatarBadge,
 } from "./avatar";
+export {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogMedia,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog";
