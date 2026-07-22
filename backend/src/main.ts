a15cbd4232deb0b4c95e725f5d09680a3064e44f
aquammarine <mrguysy@gmail.com>
2026-07-23 02:25:55 +0300

    fix: remove api docs from prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,41 +16,18 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
 
-  const config = new DocumentBuilder()
-    .setTitle('Automaze Todo API')
-    .setVersion('1.0')
-    .addCookieAuth('access_token')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document, {
-    swaggerOptions: { persistAuthorization: true },
-    customSiteTitle: 'Automaze Todo API Docs',
-    ...(process.env.SWAGGER_USER && process.env.SWAGGER_PASSWORD
-      ? {
-          httpMiddleware: (req: any, res: any, next: any) => {
-            const auth = req.headers['authorization'];
-            if (!auth) {
-              res.setHeader('WWW-Authenticate', 'Basic realm="Swagger"');
-              res.status(401).end('Unauthorized');
-              return;
-            }
-            const [, encoded] = auth.split(' ');
-            const [user, pass] = Buffer.from(encoded, 'base64')
-              .toString()
-              .split(':');
-            if (
-              user === process.env.SWAGGER_USER &&
-              pass === process.env.SWAGGER_PASSWORD
-            ) {
-              next();
-            } else {
-              res.setHeader('WWW-Authenticate', 'Basic realm="Swagger"');
-              res.status(401).end('Unauthorized');
-            }
-          },
-        }
-      : {}),
-  });
+  if(process.env.NODE_ENV !== 'production'){
+    const config = new DocumentBuilder()
+      .setTitle('Automaze Todo API')
+      .setVersion('1.0')
+      .addCookieAuth('access_token')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document, {
+      swaggerOptions: { persistAuthorization: true },
+      customSiteTitle: 'Automaze Todo API Docs',
+    });
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
